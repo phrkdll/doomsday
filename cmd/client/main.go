10cc5c9062cf81b1f6363e2ec08cb6223b88dfe8
phrkdll <b.hotop@proton.me>
2025-07-30 10:09:55 +0000

    add bubbletea package to client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,7 +1,34 @@
 package main
 
-import "fmt"
+import (
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/phrkdll/must/pkg/must"
+)
 
 func main() {
-	fmt.Println("Initial client main function...")
+	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	must.Return(p.Run()).ElsePanic()
+}
+
+type model struct {
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q", "esc":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return "Hello, Bubble Tea!"
 }
